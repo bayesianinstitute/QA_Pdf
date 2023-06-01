f1f67875c97991c336e5f91ee935929f41feac95
faizack <faizack619@gmail.com>
2023-06-01 06:58:56 -0700

    added openai error

--- Pdf-QA-Admin.py
+++ Pdf-QA-Admin.py
@@ -19,6 +19,8 @@ from langchain.text_splitter import CharacterTextSplitter
 import os
 import re
 
+from openai.error import AuthenticationError
+
 def ask_question(question, qa, chat_history):
     try:
         result = qa({"question": question, "chat_history": chat_history})
@@ -117,146 +119,150 @@ def main():
         st.success("Chat history downloaded successfully!")
 
     if api_key:
+            
+        try    :
         
-        embedding, db = load_embeddings(api_key)
-        
+            embedding, db = load_embeddings(api_key)
+            
 
-        with st.sidebar.expander("File Uploader") :
+            with st.sidebar.expander("File Uploader") :
 
-            uploaded_files = st.file_uploader(
-                "Choose PDF files", type=["pdf"], accept_multiple_files=True
-            )
-
-        if uploaded_files:
-                
-            # Create a folder to save the uploaded files
-            folder_name = "useruploaded"
-            os.makedirs(folder_name, exist_ok=True)
-
-            # Save the uploaded files to the folder
-            for file in uploaded_files:
-                file_path = os.path.join(folder_name, file.name)
-                with open(file_path, "wb") as f:
-                    f.write(file.getbuffer())
-            db = process_uploaded_files(uploaded_files,api_key)
-
-        retriever = db.as_retriever()
-        model = OpenAI(temperature=0, openai_api_key=api_key)
-
-        if 'history' not in st.session_state:
-            st.session_state['history'] = []
-
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Hello ! Ask me anything about Blockchain 🤗"]
-
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey ! 👋"]
-
-        if 'memory' not in st.session_state:
-            st.session_state.memory = ConversationBufferMemory(
-                memory_key="chat_history", return_messages=True
+                uploaded_files = st.file_uploader(
+                    "Choose PDF files", type=["pdf"], accept_multiple_files=True
                 )
 
-
-        question_generator = LLMChain(llm=model, prompt=CONDENSE_QUESTION_PROMPT)
-     
-        doc_chain = load_qa_with_sources_chain(llm=model, chain_type="stuff")
-
-        chain = ConversationalRetrievalChain(
-            retriever=retriever,
-            memory=st.session_state.memory,
-            
-            question_generator=question_generator,
-            combine_docs_chain=doc_chain,
-        )
-
-            #container for the chat history
-        response_container = st.container()
-        #container for the user's text input
-        container = st.container()
-        
-
-        def conversational_chat(query):
-            try:
-        
-                with get_openai_callback() as cb:
-                    result = chain({"question": query, "chat_history": st.session_state['history']})
-                    reference_Doc = db.similarity_search(query)
-                    answer = re.sub(r'SOURCES:.*', '', result["answer"])
+            if uploaded_files:
                     
+                # Create a folder to save the uploaded files
+                folder_name = "useruploaded"
+                os.makedirs(folder_name, exist_ok=True)
 
-                
-                    st.session_state['history'].append((query, answer))
-                   
-                    return answer,reference_Doc
-                
-            
-            except InvalidRequestError:
-                st.write("Try another chain type as the token size is larger for this chain type")
+                # Save the uploaded files to the folder
+                for file in uploaded_files:
+                    file_path = os.path.join(folder_name, file.name)
+                    with open(file_path, "wb") as f:
+                        f.write(file.getbuffer())
+                db = process_uploaded_files(uploaded_files,api_key)
+
+            retriever = db.as_retriever()
+            model = OpenAI(temperature=0, openai_api_key=api_key)
+
+            if 'history' not in st.session_state:
+                st.session_state['history'] = []
+
+            if 'generated' not in st.session_state:
+                st.session_state['generated'] = ["Hello ! Ask me anything about Blockchain 🤗"]
+
+            if 'past' not in st.session_state:
+                st.session_state['past'] = ["Hey ! 👋"]
+
+            if 'memory' not in st.session_state:
+                st.session_state.memory = ConversationBufferMemory(
+                    memory_key="chat_history", return_messages=True
+                    )
+
+
+            question_generator = LLMChain(llm=model, prompt=CONDENSE_QUESTION_PROMPT)
         
+            doc_chain = load_qa_with_sources_chain(llm=model, chain_type="stuff")
 
-        # Allow to download as well
-        download_str = []
-        query=""
-        with container:
+            chain = ConversationalRetrievalChain(
+                retriever=retriever,
+                memory=st.session_state.memory,
+                
+                question_generator=question_generator,
+                combine_docs_chain=doc_chain,
+            )
+
+                #container for the chat history
+            response_container = st.container()
+            #container for the user's text input
+            container = st.container()
             
-            with st.form(key='my_form', clear_on_submit=True):
-                
-                user_input = st.text_input("Query:", placeholder="Type Your Query (:", key='input')
-                submit_button = st.form_submit_button(label='Send',type='primary')
 
-
-            if submit_button and user_input:
-                output,reference_Doc = conversational_chat(user_input)
-                output, reference_Doc = conversational_chat(user_input)
-                print(output)
-                if output.strip() == "I don't know.":
-                    output = "I'm still learning and don't have the information you're looking for."
-                print(output)
-                
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
-                
-                
-                if st.session_state['generated']:
-                    with response_container:
-                        for i in range(len(st.session_state['generated'])):
-                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="personas")
-                            message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
-                            download_str.append(st.session_state["past"][i])
-                            download_str.append(st.session_state["generated"][i])
-                
-                with st.expander("Show Reference Documents:"):
-                    # reference_Doc = db.similarity_search(user_input)
-                        # docs = [...]  # Your list of documents
-                    refercemetadata = []  # Empty list to store metadata
-                    refercepages=[]
-                    for i in range(len(reference_Doc)):
-
-                        refercemetadata.append(reference_Doc[i].metadata)
-                        refercepages.append(reference_Doc[i].page_content)
-
-                    st.write(refercemetadata)
+            def conversational_chat(query):
+                try:
             
+                    with get_openai_callback() as cb:
+                        result = chain({"question": query, "chat_history": st.session_state['history']})
+                        reference_Doc = db.similarity_search(query)
+                        answer = re.sub(r'SOURCES:.*', '', result["answer"])
+                        
+
+                    
+                        st.session_state['history'].append((query, answer))
+                    
+                        return answer,reference_Doc
+                    
+                
+                except InvalidRequestError:
+                    st.write("Try another chain type as the token size is larger for this chain type")
             
-            # download_str = '\n'.join(download_str)
-            # if download_str:
-            #         st.sidebar.download_button('Download Conversion',download_str)
 
-            # with st.sidebar.expander("**View Chat History**"):
-            #     display_chat_history(st.session_state.history)
+            # Allow to download as well
+            download_str = []
+            query=""
+            with container:
+                
+                with st.form(key='my_form', clear_on_submit=True):
+                    
+                    user_input = st.text_input("Query:", placeholder="Type Your Query (:", key='input')
+                    submit_button = st.form_submit_button(label='Send',type='primary')
 
 
-            if st.session_state.history:   
-                if st.button("Clear-all",help="Clear all chat"):
-                    st.session_state.history=[]
+                if submit_button and user_input:
+                    output,reference_Doc = conversational_chat(user_input)
+                    output, reference_Doc = conversational_chat(user_input)
+                    print(output)
+                    if output.strip() == "I don't know.":
+                        output = "I'm still learning and don't have the information you're looking for."
+                    print(output)
+                    
+                    st.session_state['past'].append(user_input)
+                    st.session_state['generated'].append(output)
+                    
+                    
+                    if st.session_state['generated']:
+                        with response_container:
+                            for i in range(len(st.session_state['generated'])):
+                                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="personas")
+                                message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
+                                download_str.append(st.session_state["past"][i])
+                                download_str.append(st.session_state["generated"][i])
+                    
+                    with st.expander("Show Reference Documents:"):
+                        # reference_Doc = db.similarity_search(user_input)
+                            # docs = [...]  # Your list of documents
+                        refercemetadata = []  # Empty list to store metadata
+                        refercepages=[]
+                        for i in range(len(reference_Doc)):
 
-            # st.session_state.me
-            st.button("New Chat", on_click = new_chat, type='primary')
-            # if st.button("Download Chat History", help="Download chat history as a text file"):
-            #     download_chat_history()
+                            refercemetadata.append(reference_Doc[i].metadata)
+                            refercepages.append(reference_Doc[i].page_content)
 
-            
+                        st.write(refercemetadata)
+                
+                
+                # download_str = '\n'.join(download_str)
+                # if download_str:
+                #         st.sidebar.download_button('Download Conversion',download_str)
+
+                # with st.sidebar.expander("**View Chat History**"):
+                #     display_chat_history(st.session_state.history)
+
+
+                if st.session_state.history:   
+                    if st.button("Clear-all",help="Clear all chat"):
+                        st.session_state.history=[]
+
+                # st.session_state.me
+                st.button("New Chat", on_click = new_chat, type='primary')
+                # if st.button("Download Chat History", help="Download chat history as a text file"):
+                #     download_chat_history()
+
+        except AuthenticationError as e :
+            link = "[Click here](https://platform.openai.com/account/api-keys)"
+            st.error(f"Ensure the API key used is correct, clear your browser cache, or generate a new one {link}")   
 
 
     else :
--- Pdf-QA-User.py
+++ Pdf-QA-User.py
@@ -12,7 +12,7 @@ from langchain.callbacks import get_openai_callback
 import psutil
 from langchain.document_loaders import PyPDFDirectoryLoader
 import os
-
+from openai.error import AuthenticationError
 
 
 def get_memory_usage():
@@ -108,139 +108,147 @@ def main():
 
     if api_key:
 
-        embedding, db = load_embeddings(api_key)
-        
+        try :
 
-        with st.sidebar.expander("File Uploader") :
+            embedding, db = load_embeddings(api_key)
+            
 
-            uploaded_files = st.file_uploader(
-                "Choose PDF files", type=["pdf"], accept_multiple_files=True
+            with st.sidebar.expander("File Uploader") :
+
+                uploaded_files = st.file_uploader(
+                    "Choose PDF files", type=["pdf"], accept_multiple_files=True
+                )
+
+
+            if uploaded_files:
+                folder_name = "useruploaded"
+                os.makedirs(folder_name, exist_ok=True)
+
+                # Save the uploaded files to the folder
+                for file in uploaded_files:
+                    file_path = os.path.join(folder_name, file.name)
+                    with open(file_path, "wb") as f:
+                        f.write(file.getbuffer())
+                db = process_uploaded_files(uploaded_files,api_key)
+
+            retriever = db.as_retriever()
+            model = OpenAI(temperature=0, openai_api_key=api_key)
+
+            if 'history' not in st.session_state:
+                st.session_state['history'] = []
+
+            if 'generated' not in st.session_state:
+                st.session_state['generated'] = ["Hello ! Ask me anything about Blockchain 🤗"]
+
+            if 'past' not in st.session_state:
+                st.session_state['past'] = ["Hey ! 👋"]
+
+            if 'memory' not in st.session_state:
+                st.session_state.memory = ConversationBufferMemory(
+                    memory_key="chat_history", return_messages=True
+                    )
+            # st.session_state.me
+
+            
+
+            chain = ConversationalRetrievalChain.from_llm(
+                llm=model, memory=st.session_state.memory, retriever=retriever, chain_type="stuff"
             )
 
+                #container for the chat history
+            response_container = st.container()
+            #container for the user's text input
+            container = st.container()
 
-        if uploaded_files:
-            folder_name = "useruploaded"
-            os.makedirs(folder_name, exist_ok=True)
-
-            # Save the uploaded files to the folder
-            for file in uploaded_files:
-                file_path = os.path.join(folder_name, file.name)
-                with open(file_path, "wb") as f:
-                    f.write(file.getbuffer())
-            db = process_uploaded_files(uploaded_files,api_key)
-
-        retriever = db.as_retriever()
-        model = OpenAI(temperature=0, openai_api_key=api_key)
-
-        if 'history' not in st.session_state:
-            st.session_state['history'] = []
-
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Hello ! Ask me anything about Blockchain 🤗"]
-
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey ! 👋"]
-
-        if 'memory' not in st.session_state:
-            st.session_state.memory = ConversationBufferMemory(
-                memory_key="chat_history", return_messages=True
-                )
-        # st.session_state.me
-
-        
-
-        chain = ConversationalRetrievalChain.from_llm(
-            llm=model, memory=st.session_state.memory, retriever=retriever, chain_type="stuff"
-        )
-
-            #container for the chat history
-        response_container = st.container()
-        #container for the user's text input
-        container = st.container()
-
-        def conversational_chat(query):
-            try:
-        
-                with get_openai_callback() as cb:
-                    result = chain({"question": query, "chat_history": st.session_state['history']})
-                    st.session_state['history'].append((query, result["answer"]))
-                    # expander = st.sidebar.expander("Token Details")
-
-                    # if expander.button("Show token"):
-                    #     expander.write(f"Total Tokens: {cb.total_tokens}")
-                    #     expander.write(f"Prompt Tokens: {cb.prompt_tokens}")
-                    #     expander.write(f"Completion Tokens: {cb.completion_tokens}")
-                    #     expander.write(f"Total Cost (USD): ${cb.total_cost}")
-                                
-                    return result["answer"]
-                
+            def conversational_chat(query):
+                try:
             
-            except InvalidRequestError:
-                st.write("Try another chain type as the token size is larger for this chain type")
-        
+                    with get_openai_callback() as cb:
+                        result = chain({"question": query, "chat_history": st.session_state['history']})
+                        st.session_state['history'].append((query, result["answer"]))
+                        # expander = st.sidebar.expander("Token Details")
 
-        # Allow to download as well
-        download_str = []
-        with container:
-            
-            with st.form(key='my_form', clear_on_submit=True):
-                
-                user_input = st.text_input("Query:", placeholder="Type Your Query (:", key='input')
-                submit_button = st.form_submit_button(label='Send',type='primary')
-                
-            if submit_button and user_input:
-                output = conversational_chat(user_input)
-
-                print(output)
-                if output.strip() == "I don't know.":
-                    output = "I'm still in the process of learning, and I may not have the exact information you're seeking. However, I'll do my best to assist you with any questions or concerns you have. Please let me know how I can be of help to you."
-                print(output)
-
-                
+                        # if expander.button("Show token"):
+                        #     expander.write(f"Total Tokens: {cb.total_tokens}")
+                        #     expander.write(f"Prompt Tokens: {cb.prompt_tokens}")
+                        #     expander.write(f"Completion Tokens: {cb.completion_tokens}")
+                        #     expander.write(f"Total Cost (USD): ${cb.total_cost}")
+                                    
+                        return result["answer"]
                     
                 
-                
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
-                
-                
-                if st.session_state['generated']:
-                    with response_container:
-                        for i in range(len(st.session_state['generated'])):
-                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="personas")
-                            message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
-                            download_str.append(st.session_state["past"][i])
-                            download_str.append(st.session_state["generated"][i])
-                
-                
-    
+                except InvalidRequestError:
+                    st.write("Try another chain type as the token size is larger for this chain type")
             
-            
-            # download_str = '\n'.join(download_str)
-            # if download_str:
-            #         st.sidebar.download_button('Download Conversion',download_str)
 
-            # with st.sidebar.expander("**View Chat History**"):
-            #     display_chat_history(st.session_state.history)
+            # Allow to download as well
+            download_str = []
+            with container:
+                
+                with st.form(key='my_form', clear_on_submit=True):
+                    
+                    user_input = st.text_input("Query:", placeholder="Type Your Query (:", key='input')
+                    submit_button = st.form_submit_button(label='Send',type='primary')
+                    
+                if submit_button and user_input:
+                    output = conversational_chat(user_input)
 
-            if st.session_state.history:   
-                if st.button("Clear-all",help="Clear all chat"):
-                    st.session_state.history=[]
+                    print(output)
+                    if output.strip() == "I don't know.":
+                        output = "I'm still in the process of learning, and I may not have the exact information you're seeking. However, I'll do my best to assist you with any questions or concerns you have. Please let me know how I can be of help to you."
+                    print(output)
 
-            st.button("New Chat", on_click = new_chat, type='primary')
-            
-            # Allow the user to clear all stored conversation sessions
-        # if st.button("Download Chat History"):
-        #     # Create a string representation of the chat history
-        #     chat_history_str = "\n".join([f"Question {i+1}: {q}\nAnswer {i+1}: {a}\n----------" for i, (q, a) in enumerate(st.session_state.memory)])
+                    
+                        
+                    
+                    
+                    st.session_state['past'].append(user_input)
+                    st.session_state['generated'].append(output)
+                    
+                    
+                    if st.session_state['generated']:
+                        with response_container:
+                            for i in range(len(st.session_state['generated'])):
+                                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="personas")
+                                message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
+                                download_str.append(st.session_state["past"][i])
+                                download_str.append(st.session_state["generated"][i])
+                    
+                    
+        
+                
+                
+                # download_str = '\n'.join(download_str)
+                # if download_str:
+                #         st.sidebar.download_button('Download Conversion',download_str)
 
-        #     # Save the chat history to a text file
-        #     with open("QA_chat_history.txt", "w") as file:
-        #         file.write(chat_history_str)
+                # with st.sidebar.expander("**View Chat History**"):
+                #     display_chat_history(st.session_state.history)
 
-        #     st.success("Chat history downloaded successfully!")
-        memory_usage = get_memory_usage()
+                if st.session_state.history:   
+                    if st.button("Clear-all",help="Clear all chat"):
+                        st.session_state.history=[]
+
+                st.button("New Chat", on_click = new_chat, type='primary')
+                
+                # Allow the user to clear all stored conversation sessions
+            # if st.button("Download Chat History"):
+            #     # Create a string representation of the chat history
+            #     chat_history_str = "\n".join([f"Question {i+1}: {q}\nAnswer {i+1}: {a}\n----------" for i, (q, a) in enumerate(st.session_state.memory)])
+
+            #     # Save the chat history to a text file
+            #     with open("QA_chat_history.txt", "w") as file:
+            #         file.write(chat_history_str)
+
+            #     st.success("Chat history downloaded successfully!")
+            memory_usage = get_memory_usage()
         # st.write(f"Memory usage: {memory_usage} MB")
+        except AuthenticationError as e :
+            link = "[Click here](https://platform.openai.com/account/api-keys)"
+            st.error(f"Ensure the API key used is correct, clear your browser cache, or generate a new one {link}")
+
+
+
     else :
         st.sidebar.warning("Please Enter Api Key")    
 
